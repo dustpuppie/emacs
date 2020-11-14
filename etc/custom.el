380f1e22e1ac635f3298f0592d2f04ad8a4c97b7
Dan Baggeroer <dbaggero@protonmail.com>
2020-11-14 17:06:27 -0600

    move org-src-window-setup to config.el

--- etc/custom.el
+++ etc/custom.el
@@ -3,10 +3,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-src-window-setup (quote split-window-below))
  '(package-selected-packages
    (quote
-    (counsel-projectile doom-modeline all-the-icons yaml-mode ws-butler which-key vscode-dark-plus-theme use-package undo-tree switch-window smartparens sdcv ripgrep projectile org-bullets org no-littering multiple-cursors magit json-mode ivy-rich highlight-numbers highlight-escape-sequences go-mode gcmh flycheck exec-path-from-shell eglot diredfl dired-rainbow dired-filter dired-avfs delight dash-functional counsel-dash company beacon))))
+    (doom-themes yaml-mode ws-butler which-key vscode-dark-plus-theme use-package undo-tree switch-window smartparens ripgrep org no-littering multiple-cursors magit json-mode ivy-rich highlight-numbers highlight-escape-sequences helpful go-mode gcmh exec-path-from-shell doom-modeline diredfl dired-rainbow dired-filter dired-avfs delight counsel-projectile counsel-dash beacon))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
