4d77182ce7eb73dbc435f10fe88ee04aa124bfe3
Dan Baggeroer <dbaggero@protonmail.com>
2020-11-13 18:41:53 -0600

    initial commit

---
+++ etc/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-src-window-setup (quote split-window-below))
+ '(package-selected-packages
+   (quote
+    (counsel-projectile doom-modeline all-the-icons yaml-mode ws-butler which-key vscode-dark-plus-theme use-package undo-tree switch-window smartparens sdcv ripgrep projectile org-bullets org no-littering multiple-cursors magit json-mode ivy-rich highlight-numbers highlight-escape-sequences go-mode gcmh flycheck exec-path-from-shell eglot diredfl dired-rainbow dired-filter dired-avfs delight dash-functional counsel-dash company beacon))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ init.el
@@ -0,0 +1,56 @@
+;;; package --- dustpuppie's emacs.d init file -*- lexical-binding: t -*-
+;;; Commentary:
+;;; Copyright (c) 2004-2020 dustpuppie
+;;; Code:
+
+;;_____________________________________________________________________________
+;; Debugging
+;;_____________________________________________________________________________
+(setq debug-on-error t)
+(setq debug-on-quit t)
+(add-hook 'after-init-hook '(lambda ()
+			      (setq debug-on-error nil
+				    debug-on-quit  nil)))
+
+(require 'cl)
+(require 'package)
+;; Add some repositories
+(add-to-list 'package-archives
+	     '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+;;_____________________________________________________________________________
+;; do some stuff to make sure use-package et al are installed
+;;_____________________________________________________________________________
+(when (not package-archive-contents)
+  (package-refresh-contents))
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(unless (package-installed-p 'delight)
+  (package-refresh-contents)
+  (package-install 'delight))
+
+(eval-and-compile
+  (require 'bind-key)
+  (require 'use-package nil t))
+
+;;_____________________________________________________________________________
+;; Change Scratch message
+;;_____________________________________________________________________________
+(setq initial-scratch-message ";; ╔═╗┌─┐┬─┐┌─┐┌┬┐┌─┐┬ ┬\n;; ╚═╗│  ├┬┘├─┤ │ │  ├─┤\n;; ╚═╝└─┘┴└─┴ ┴ ┴ └─┘┴ ┴\n")
+
+;;_____________________________________________________________________________
+;; start emacs server
+;;_____________________________________________________________________________
+(server-start)
+;;_____________________________________________________________________________
+;; Tangle config.org file to make config.el
+;;_____________________________________________________________________________
+(when (file-readable-p "~/.emacs.d/config.org")
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
+(load custom-file)
+(provide 'init)
+;;; init.el ends here
