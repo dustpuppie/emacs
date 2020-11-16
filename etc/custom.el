305724c7fafdf28a3e63bd64ada19de370d74a04
Dan Baggeroer <dbaggero@protonmail.com>
2020-11-16 07:26:46 -0600

    standard changes

--- etc/custom.el
+++ etc/custom.el
@@ -5,10 +5,19 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (doom-themes yaml-mode ws-butler which-key vscode-dark-plus-theme use-package undo-tree switch-window smartparens ripgrep org no-littering multiple-cursors magit json-mode ivy-rich highlight-numbers highlight-escape-sequences helpful go-mode gcmh exec-path-from-shell doom-modeline diredfl dired-rainbow dired-filter dired-avfs delight counsel-projectile counsel-dash beacon))))
+    (smart-mode-line-powerline-theme powerline company smart-mode-line doom-themes yaml-mode ws-butler which-key vscode-dark-plus-theme use-package undo-tree switch-window smartparens ripgrep org no-littering multiple-cursors magit json-mode ivy-rich highlight-numbers highlight-escape-sequences helpful go-mode gcmh exec-path-from-shell doom-modeline diredfl dired-rainbow dired-filter dired-avfs delight counsel-projectile counsel-dash beacon)))
+ '(powerline-default-separator (quote wave))
+ '(powerline-display-buffer-size t)
+ '(powerline-display-hud t)
+ '(powerline-display-mule-info nil)
+ '(powerline-gui-use-vcs-glyph t)
+ '(powerline-height 35))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(mode-line ((t (:height 0.85))))
+ '(mode-line-inactive ((t (:height 0.85))))
+ '(powerline-inactive0 ((t (:background "dark blue"))))
+ '(powerline-inactive1 ((t (:background "#272a39")))))
