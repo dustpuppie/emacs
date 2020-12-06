d75970484481f7fe316b21959fee8676ff5856b7
Daniel Baggeroer <dbaggero@Molly.local>
2020-12-05 18:21:43 -0600

    package-selected-packages

--- etc/custom.el
+++ etc/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(dired-single eglot lsp-ui typescript-mode lsp-mode all-the-icons-dired go-gopath go-guru go-imports auto-sudoedit eshell-git-prompt vterm eterm-256color mode-icons ob-go org-superstar yaml-mode json-mode go-mode company highlight-escape-sequences highlight-numbers smartparens ws-butler magit counsel-projectile projectile counsel-dash dired-rainbow dired-filter diredfl undo-tree switch-window multiple-cursors ripgrep ivy-posframe ivy-rich counsel helpful beacon powerline all-the-icons smart-mode-line vscode-dark-plus-theme no-littering gcmh delight exec-path-from-shell which-key use-package)))
+   '(pdf-tools dired-single eglot lsp-ui typescript-mode lsp-mode all-the-icons-dired go-gopath go-guru go-imports auto-sudoedit eshell-git-prompt vterm eterm-256color mode-icons ob-go org-superstar yaml-mode json-mode go-mode company highlight-escape-sequences highlight-numbers smartparens ws-butler magit counsel-projectile projectile counsel-dash dired-rainbow dired-filter diredfl undo-tree switch-window multiple-cursors ripgrep ivy-posframe ivy-rich counsel helpful beacon powerline all-the-icons smart-mode-line vscode-dark-plus-theme no-littering gcmh delight exec-path-from-shell which-key use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
