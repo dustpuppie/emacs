226ba804b1474dc6f9ad5258b89b02511ef0172b
Daniel Baggeroer <dbaggero@Molly-4.local>
2022-05-22 16:21:54 -0500

    minor changes

--- etc/custom.el
+++ etc/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(treemacs-magit tramp company-go dockerfile-mode docker company-lsp edwina lsp-treemacs lsp-ivy company-box pdf-tools dired-single eglot lsp-ui typescript-mode lsp-mode all-the-icons-dired go-gopath go-guru go-imports auto-sudoedit eshell-git-prompt eterm-256color mode-icons ob-go org-superstar json-mode go-mode highlight-escape-sequences highlight-numbers ws-butler magit counsel-projectile counsel-dash dired-rainbow dired-filter diredfl undo-tree multiple-cursors ripgrep ivy-posframe ivy-rich counsel helpful beacon powerline all-the-icons no-littering gcmh delight exec-path-from-shell use-package)))
+   '(smart-mode-line smart-mode-line-powerline-theme treemacs-magit tramp company-go dockerfile-mode docker company-lsp edwina lsp-treemacs lsp-ivy company-box pdf-tools dired-single eglot lsp-ui typescript-mode lsp-mode all-the-icons-dired go-gopath go-guru go-imports auto-sudoedit eshell-git-prompt eterm-256color mode-icons ob-go org-superstar json-mode go-mode highlight-escape-sequences highlight-numbers ws-butler magit counsel-projectile counsel-dash dired-rainbow dired-filter diredfl undo-tree multiple-cursors ripgrep ivy-posframe ivy-rich counsel helpful beacon powerline all-the-icons no-littering gcmh delight exec-path-from-shell use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
