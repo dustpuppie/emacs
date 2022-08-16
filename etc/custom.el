ea50291eb509039b69f70804e6c5f9572547a080
Daniel Baggeroer <dbaggero@Molly-4.local>
2022-08-16 12:11:02 -0500

    changes as part of rewrite

--- etc/custom.el
+++ etc/custom.el
@@ -4,10 +4,20 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(smart-mode-line smart-mode-line-powerline-theme treemacs-magit tramp company-go dockerfile-mode docker company-lsp edwina lsp-treemacs lsp-ivy company-box pdf-tools dired-single eglot lsp-ui typescript-mode lsp-mode all-the-icons-dired go-gopath go-guru go-imports auto-sudoedit eshell-git-prompt eterm-256color mode-icons ob-go org-superstar json-mode go-mode highlight-escape-sequences highlight-numbers ws-butler magit counsel-projectile counsel-dash dired-rainbow dired-filter diredfl undo-tree multiple-cursors ripgrep ivy-posframe ivy-rich counsel helpful beacon powerline all-the-icons no-littering gcmh delight exec-path-from-shell use-package)))
+   '(dracula-theme github-dark-vscode-theme vscode-dark-plus-theme exec-path-from-shell mode-icons faff-theme bubbleberry-theme undo-fu package-utils hydra el-get leaf-keywords)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(aw-leading-char-face ((t (:height 4.0 :foreground "#f1fa8c"))) nil "Customized with leaf in `ace-window' block at `/Users/dbaggero/.emacs.d/config.el'")
+ '(company-template-field ((t (:foreground "#ff79c6"))) nil "Customized with leaf in `company' block at `/Users/dbaggero/.emacs.d/config.el'")
+ '(company-tooltip ((t (:background "#323445"))) nil "Customized with leaf in `company' block at `/Users/dbaggero/.emacs.d/config.el'")
+ '(flyspell-duplicate ((t (:underline (:color "#50fa7b" :style wave)))) nil "Customized with leaf in `flyspell' block at `/Users/dbaggero/.emacs.d/config.el'")
+ '(flyspell-incorrect ((t (:underline (:color "#f1fa8c" :style wave)))) nil "Customized with leaf in `flyspell' block at `/Users/dbaggero/.emacs.d/config.el'")
+ '(git-gutter:added ((t (:foreground "#50fa7b"))) nil "Customized with leaf in `git-gutter' block at `/Users/dbaggero/.emacs.d/config.el'")
+ '(git-gutter:deleted ((t (:foreground "#ff79c6"))) nil "Customized with leaf in `git-gutter' block at `/Users/dbaggero/.emacs.d/config.el'")
+ '(git-gutter:modified ((t (:foreground "#f1fa8c"))) nil "Customized with leaf in `git-gutter' block at `/Users/dbaggero/.emacs.d/config.el'")
+ '(marginalia-documentation ((t (:foreground "#6272a4"))) nil "Customized with leaf in `marginalia' block at `/Users/dbaggero/.emacs.d/config.el'")
+ '(vertico-postframe-border ((t (:background "#323445"))) nil "Customized with leaf in `vertico-posframe' block at `/Users/dbaggero/.emacs.d/config.el'")
+ '(yas-field-highlight-face ((t (:foreground "#ff79c6"))) nil "Customized with leaf in `company' block at `/Users/dbaggero/.emacs.d/config.el'"))
