3c50661f0808d7e569a77ab643148b508cba63ea
Daniel Baggeroer <dbaggero@Molly-4.local>
2022-08-16 12:08:23 -0500

    cleanup as part of major re-write

--- init.el
+++ init.el
@@ -4,41 +4,6 @@
 ;;; Code:
 
 ;;_____________________________________________________________________________
-;; Debugging
-;;_____________________________________________________________________________
-(setq debug-on-error t)
-(setq debug-on-quit t)
-(add-hook 'after-init-hook '(lambda ()
-			      (setq debug-on-error nil
-				    debug-on-quit  nil)))
-
-;; (require 'cl)
-(require 'project)
-(require 'package)
-;; Add some repositories
-(add-to-list 'package-archives
-	     '("org" . "http://orgmode.org/elpa/") t)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/") t)
-(package-initialize)
-
-;;_____________________________________________________________________________
-;; do some stuff to make sure use-package et al are installed
-;;_____________________________________________________________________________
-(when (not package-archive-contents)
-  (package-refresh-contents))
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-(unless (package-installed-p 'delight)
-  (package-refresh-contents)
-  (package-install 'delight))
-
-(eval-and-compile
-  (require 'bind-key)
-  (require 'use-package nil t))
-
-;;_____________________________________________________________________________
 ;; Change Scratch message
 ;;_____________________________________________________________________________
 (setq initial-scratch-message ";; ╔═╗┌─┐┬─┐┌─┐┌┬┐┌─┐┬ ┬\n;; ╚═╗│  ├┬┘├─┤ │ │  ├─┤\n;; ╚═╝└─┘┴└─┴ ┴ ┴ └─┘┴ ┴\n")
